0321ac08cf93876d471fbe8aa9c5016a9a5270c2
Fernando Michelotti <fernando.michelotti@gmail.com>
2010-04-15 15:47:29 -0300

    Adicionado arquivo install para instalacao via http

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O /usr/lib/bonobo/servers/youtube-ffmpeg.server  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.server
+wget -O /usr/local/bin/youtube-ffmpeg.py  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.py
+wget -O /usr/share/pixmaps/YouTubeFFMPEG.svg  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
+
