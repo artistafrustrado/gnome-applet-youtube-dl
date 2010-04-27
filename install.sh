d5e83aeb5e466e3a9524d3a4b30544d11624e74b
Fernando Michelotti <artista@frustrado.com.br>
2010-04-27 10:04:17 -0300

    Added png version of icon

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 wget -O /usr/lib/bonobo/servers/youtube-ffmpeg.server  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.server
 wget -O /usr/local/bin/youtube-ffmpeg.py  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.py
 wget -O /usr/share/pixmaps/YouTubeFFMPEG.svg  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
+wget -O /usr/share/pixmaps/YouTubeFFMPEG.png  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
 chmod a+x /usr/local/bin/youtube-ffmpeg.py
