3a0c65d346d96a99dcd416712cc981616ba92b93
Fernando Michelotti <artista@frustrado.com.br>
2010-11-24 12:01:23 -0200

    updated wget no to trust github.com certificates

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget -O /usr/lib/bonobo/servers/youtube-ffmpeg.server  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.server
-wget -O /usr/local/bin/youtube-ffmpeg.py  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.py
-wget -O /usr/share/pixmaps/YouTubeFFMPEG.svg  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
-wget -O /usr/share/pixmaps/YouTubeFFMPEG.png  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
-wget -O /usr/local/bin/youtube-dlf http://bitbucket.org/rg3/youtube-dl/raw/6bb800654cbb/youtube-dl
+wget --no-check-certificate -O /usr/lib/bonobo/servers/youtube-ffmpeg.server  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.server
+wget --no-check-certificate -O /usr/local/bin/youtube-ffmpeg.py  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/youtube-ffmpeg.py
+wget --no-check-certificate -O /usr/share/pixmaps/YouTubeFFMPEG.svg  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
+wget --no-check-certificate -O /usr/share/pixmaps/YouTubeFFMPEG.png  http://github.com/artistafrustrado/gnome-applet-youtube-dl/raw/master/src/YouTubeFFMPEG.svg
+wget --no-check-certificate -O /usr/local/bin/youtube-dlf http://bitbucket.org/rg3/youtube-dl/raw/6bb800654cbb/youtube-dl
 chmod a+x /usr/local/bin/youtube-ffmpeg.py
 chmod a+x /usr/local/bin/youtube-dlf
 
